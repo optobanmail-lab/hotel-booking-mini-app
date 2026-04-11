27730b997e8b39d0bb858f9185a5e2e230a0df95
optobanmail-lab <optoban.mail@gmail.com>
2026-04-11 08:27:41 +0500

    debug: disable transitions to prevent crash

--- frontend/src/layout/AppLayout.jsx
+++ frontend/src/layout/AppLayout.jsx
@@ -1,9 +1,6 @@
 import { Box } from '@mui/material'
 import { Outlet, useLocation } from 'react-router-dom'
-import { AnimatePresence } from 'framer-motion'
 import BottomBar from '../components/BottomBar'
-import PageTransition from '../components/PageTransition'
-import TelegramFullscreenButton from '../components/TelegramFullscreenButton'
 
 export default function AppLayout() {
     const location = useLocation()
@@ -11,29 +8,9 @@ export default function AppLayout() {
 
     return (
         <Box sx={{ minHeight: '100dvh', display: 'flex', flexDirection: 'column' }}>
-            {/* Кнопка поверх интерфейса (лучше скрыть в админке) */}
-            {!isAdmin && (
-                <Box
-                    sx={{
-                        position: 'fixed',
-                        right: 12,
-                        top: 'calc(env(safe-area-inset-top) + 12px)',
-                        zIndex: 9999,
-                    }}
-                >
-                    <TelegramFullscreenButton />
-                </Box>
-            )}
-
-            {/* Контент */}
             <Box sx={{ flex: 1, pb: isAdmin ? 0 : 11, overflowX: 'hidden' }}>
-                <AnimatePresence mode="sync" initial={false}>
-                    <PageTransition key={location.pathname}>
-                        <Outlet />
-                    </PageTransition>
-                </AnimatePresence>
+                <Outlet />
             </Box>
-
             {!isAdmin && <BottomBar />}
         </Box>
     )
